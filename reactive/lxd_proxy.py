df80519b61c0d5665966d0c462b8289408d571bc
jamesbeedy <jamesbeedy@gmail.com>
2017-06-02 16:22:15 +0000

    simplify reactive logic

--- reactive/lxd_proxy.py
+++ reactive/lxd_proxy.py
@@ -1,12 +1,7 @@
-from charms.reactive import when_not, set_state, when
+from charms.reactive import when_not
 from charmhelpers.core.hookenv import status_set
 
 
-@when_not('lxd.proxy.available')
-def init_proxy():
-    set_state('lxd.proxy.available')
-
-
-@when('lxd.proxy.available')
+@when_not('lxd.proxy.not.available')
 def status_set_avail():
     status_set('active', "LXD proxy available")
